4db6d390bf4e10a200fb39271430f16518e594bc
filian <filiandun@gmail.com>
2023-06-15 14:57:47 +0300

    project: 80% ready

--- HWMoney/Program.cs
+++ HWMoney/Program.cs
@@ -2,30 +2,32 @@
 {
     public class Money
     {
-        private ulong rubles;
-        private ushort pennies;
+        private long rubles;
+        private short kopeyki;
 
         public Money()
         {
             this.rubles = 0;
-            this.pennies = 0;
+            this.kopeyki = 0;
         }
 
-        public Money(ulong rubles, ushort pennies)
+        public Money(long rubles, short kopeyki)
         {
             try
             {
-                if (pennies >= 100)
+                if (kopeyki >= 100 || kopeyki < 0)
                 {
-                    throw new OverflowException($"ОШИБКА: копеек не может быть больше или равно 100! (Вы задали: {pennies})");
+                    throw new OverflowException($"ОШИБКА: копеек не может быть больше или равно 100 или меньше 0!");
                 }
 
                 this.rubles = rubles;
-                this.pennies = pennies;
+                this.kopeyki = kopeyki;
             }
-            catch (Exception ex)
+            catch (OverflowException oe)
             {
-                Console.WriteLine(ex.Message);
+                Console.WriteLine(oe.Message);
+                this.rubles = 0;
+                this.kopeyki = 0;
             }
         }
 
@@ -33,61 +35,85 @@
         {
             try
             {
-                ulong rubles = (ulong)(m.rubles - Math.Round(money));
-                ushort pennies = (ushort)(m.pennies - (money - Math.Round(money)) * 100);
+                long rubles = (long)(m.rubles - Math.Truncate(money));
+                short kopeyki = (short)(m.kopeyki - (money - Math.Truncate(money)) * 100);
 
-                return new Money(rubles, pennies);
+                if (rubles < 0 || kopeyki < 0)
+                {
+                    throw new OverflowException($"К сожалению, теперь Вы банкрот, ваша задолженность составляет {rubles} руб. {kopeyki} коп.");
+                }
+                if (rubles > long.MaxValue || rubles < long.MinValue || kopeyki > short.MaxValue || kopeyki < short.MinValue)
+                {
+                    throw new OverflowException($"Архивы нашего банка не позволяют хранить столько.");
+                }
+
+                return new Money(rubles, kopeyki);
             }
-            catch
+            catch (OverflowException oe)
             {
-                Console.ForegroundColor = ConsoleColor.Red; Console.Write("К сожалению, теперь вы банкрот"); Console.ResetColor();
-                return new Money(m.rubles, m.pennies);
+                Console.ForegroundColor = ConsoleColor.Red; Console.WriteLine(oe.Message); Console.ResetColor();
+
+                return new Money(0, 0);
             }
+            //catch (Exception ex)
+            //{
+            //    Console.WriteLine(ex.Message);
+            //    Console.WriteLine(ex.Data);
+
+            //    Console.ForegroundColor = ConsoleColor.Red; Console.WriteLine($"К сожалению, теперь Вы банкрот, ваша задолженность составляет {m.rubles} руб. {m.kopeyki - 1} коп."); Console.ResetColor();
+            //    return new Money(0, 0);
+            //}
         }
 
         public static Money operator +(Money m, decimal money)
         {
             try
             {
-                ulong rubles = (ulong)(m.rubles + Math.Round(money));
-                ushort pennies = (ushort)(m.pennies + (money - Math.Round(money)) * 100);
+                long rubles = (long)(m.rubles + Math.Truncate(money));
+                short kopeyki = (short)(m.kopeyki + (money - Math.Truncate(money)) * 100);
 
-                return new Money(rubles, pennies);
+                return new Money(rubles, kopeyki);
             }
-            catch
+            catch (OverflowException ofe)
             {
-                Console.ForegroundColor = ConsoleColor.Red; Console.Write("К сожалению, теперь Вы банкрот"); Console.ResetColor();
-                return new Money(m.rubles, m.pennies);
-            }
-        }
+                Console.WriteLine("ОНО " + ofe.Message);
 
-        public static Money operator *(Money m, ulong num)
-        {
-            try
-            {
-                ulong rubles = m.rubles * num;
-
-                return new Money(rubles, m.pennies);
+                return new Money(0, 0);
             }
-            catch
+            catch (Exception ex)
             {
-                Console.ForegroundColor = ConsoleColor.Red; Console.Write("К сожалению, теперь Вы банкрот"); Console.ResetColor();
-                return new Money(m.rubles, m.pennies);
+                Console.ForegroundColor = ConsoleColor.Red; Console.WriteLine($"К сожалению, теперь Вы банкрот, ваша задолженность составляет {m.rubles} руб. {m.kopeyki - 1} коп."); Console.ResetColor();
+                return new Money(m.rubles, m.kopeyki);
             }
         }
 
-        public static Money operator /(Money m, ulong num)
+        public static Money operator *(Money m, long num)
         {
             try
             {
-                ulong rubles = m.rubles / num;
+                long rubles = m.rubles * num;
 
-                return new Money(rubles, m.pennies);
+                return new Money(rubles, m.kopeyki);
             }
             catch
             {
-                Console.ForegroundColor = ConsoleColor.Red; Console.Write("К сожалению, теперь Вы банкрот"); Console.ResetColor();
-                return new Money(m.rubles, m.pennies);
+                Console.ForegroundColor = ConsoleColor.Red; Console.WriteLine($"К сожалению, теперь Вы банкрот, ваша задолженность составляет {m.rubles} руб. {m.kopeyki - 1} коп."); Console.ResetColor();
+                return new Money(m.rubles, m.kopeyki);
+            }
+        }
+
+        public static Money operator /(Money m, long num)
+        {
+            try
+            {
+                long rubles = m.rubles / num;
+
+                return new Money(rubles, m.kopeyki);
+            }
+            catch
+            {
+                Console.ForegroundColor = ConsoleColor.Red; Console.WriteLine($"К сожалению, теперь Вы банкрот, ваша задолженность составляет {m.rubles} руб. {m.kopeyki - 1} коп."); Console.ResetColor();
+                return new Money(m.rubles, m.kopeyki);
             }
         }
 
@@ -95,12 +121,12 @@
         {
             try
             {
-                return new Money(m.rubles, (ushort) (m.pennies + 1));
+                return new Money(m.rubles, (short) (m.kopeyki + 1));
             }
             catch
             {
-                Console.ForegroundColor = ConsoleColor.Red; Console.Write("К сожалению, теперь Вы банкрот"); Console.ResetColor();
-                return new Money(m.rubles, m.pennies);
+                Console.ForegroundColor = ConsoleColor.Red; Console.Write($"К сожалению, теперь Вы банкрот, ваша задолженность составляет {m.rubles} руб. {m.kopeyki - 1} коп."); Console.ResetColor();
+                return new Money(m.rubles, m.kopeyki);
             }
         }
 
@@ -108,18 +134,18 @@
         {
             try
             {
-                return new Money(m.rubles, (ushort) (m.pennies - 1));
+                return new Money(m.rubles, (short) (m.kopeyki - 1));
             }
             catch
             {
-                Console.ForegroundColor = ConsoleColor.Red; Console.Write("К сожалению, теперь Вы банкрот"); Console.ResetColor();
-                return new Money(m.rubles, m.pennies);
+                Console.ForegroundColor = ConsoleColor.Red; Console.WriteLine($"К сожалению, теперь Вы банкрот, ваша задолженность составляет {m.rubles} руб. {m.kopeyki - 1} коп."); Console.ResetColor();
+                return new Money(0, 0);
             }
         }
 
         public override string ToString()
         {
-            return $"{this.rubles} руб. {this.pennies} коп.";
+            return $"{this.rubles} руб. {this.kopeyki} коп.";
         }
     }
 
@@ -129,7 +155,12 @@
     {
         static void Main()
         {
-            Console.WriteLine("Hello, World!");
+            Money money = new Money(1, 0);
+            Console.WriteLine($"Изначально денег: {money}");
+
+            //money = money - 24.84m;
+            money = money - 18446744073709551615;
+            Console.WriteLine($"money - 3483: {money}");
         }
     }
 }
